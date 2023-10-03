14eb954b96b4a8d983f37e778a8a1fff381162c5
Gustavo <gumurdiga@gmail.com>
2023-10-03 14:59:20 -0300

    fix: deleted widget not used

--- src/components/scroller/index.ts
+++
@@ -1,9 +0,0 @@
-import { ScrollerContainer } from "./scroller-container";
-import { ScrollerItem } from "./scroller-item";
-import { ScrollerSection } from "./scroller-section";
-
-export const Scroller = {
-	Container: ScrollerContainer,
-	Item: ScrollerItem,
-	Section: ScrollerSection,
-};
