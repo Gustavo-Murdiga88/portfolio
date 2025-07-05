3bcc63a4ee7a598b97e40c298d69fe028e3a4e28
Gustavo Murdiga <gumurdiga@gmail.com>
2025-07-04 21:19:31 -0300

    feat: added the view transitions

--- src/app/(me)/layout.tsx
+++ src/app/(me)/layout.tsx
@@ -1,66 +1,69 @@
 import { ChevronLeft, Cross, Github, Home, Linkedin } from "lucide-react";
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 import { ReactNode } from "react";
+import { unstable_ViewTransition as ViewTransitions } from "react";
 
 import { Title } from "@/components/title";
 
 export default function MeLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-svh animate-fade-up flex-col">
-      <Title id="setup" title="Meus Equipamentos">
-        <nav
-          aria-label="Ações"
-          className="flex flex-1 items-center justify-end gap-2"
-        >
-          <Link
-            href="/"
-            title="Voltar"
-            className="mr-auto flex items-center gap-1 p-1 text-[0.75rem] text-zinc-500/90 transition-colors duration-300 hover:text-zinc-50"
-            aria-labelledby="voltar"
+    <ViewTransitions name="me" exit={"fade-out-right"} enter={"fade-right"}>
+      <div className="flex h-svh flex-col">
+        <Title id="setup" title="Meus Equipamentos">
+          <nav
+            aria-label="Ações"
+            className="flex flex-1 items-center justify-end gap-2"
           >
-            <ChevronLeft id="voltar" size={18} />
-            Voltar
-          </Link>
+            <Link
+              href="/"
+              title="Voltar"
+              className="mr-auto flex items-center gap-1 p-1 text-[0.75rem] text-zinc-500/90 transition-colors duration-300 hover:text-zinc-50"
+              aria-labelledby="voltar"
+            >
+              <ChevronLeft id="voltar" size={18} />
+              Voltar
+            </Link>
 
-          <Link
-            aria-labelledby="home"
-            href="/"
-            title="Página principal"
-            className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
-          >
-            <Home id="home" aria-label="Bíblia" size={18} />
-          </Link>
+            <Link
+              aria-labelledby="home"
+              href="/"
+              title="Página principal"
+              className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
+            >
+              <Home id="home" aria-label="Bíblia" size={18} />
+            </Link>
 
-          <Link
-            aria-labelledby="bible"
-            href="/daily-liturgy"
-            title="Liturgia diária"
-            className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
-          >
-            <Cross id="bible" aria-label="Bíblia" size={18} />
-          </Link>
+            <Link
+              aria-labelledby="bible"
+              href="/daily-liturgy"
+              title="Liturgia diária"
+              className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
+            >
+              <Cross id="bible" aria-label="Bíblia" size={18} />
+            </Link>
 
-          <Link
-            aria-labelledby="github"
-            href="https://github.com/Gustavo-Murdiga88"
-            target="_blank"
-            title="github"
-            className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
-          >
-            <Github id="github" aria-label="github" size={18} />
-          </Link>
-          <Link
-            aria-labelledby="LinkedIn"
-            href="https://www.linkedin.com/in/gustavo-murdiga-055470178/"
-            target="_blank"
-            title="LinkedIn"
-            className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
-          >
-            <Linkedin id="LinkedIn" aria-label="LinkedIn" size={18} />
-          </Link>
-        </nav>
-      </Title>
-      {children}
-    </div>
+            <Link
+              aria-labelledby="github"
+              href="https://github.com/Gustavo-Murdiga88"
+              target="_blank"
+              title="github"
+              className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
+            >
+              <Github id="github" aria-label="github" size={18} />
+            </Link>
+            <Link
+              aria-labelledby="LinkedIn"
+              href="https://www.linkedin.com/in/gustavo-murdiga-055470178/"
+              target="_blank"
+              title="LinkedIn"
+              className="rounded-xs p-2 text-zinc-50 transition-colors duration-300 hover:bg-zinc-700/30 focus-visible:ring-2 focus-visible:ring-neutral-100"
+            >
+              <Linkedin id="LinkedIn" aria-label="LinkedIn" size={18} />
+            </Link>
+          </nav>
+        </Title>
+        {children}
+      </div>
+    </ViewTransitions>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "@/lib/nodemailer";
 
 import { Viewport } from "next";
 import { Poppins } from "next/font/google";
-import { ViewTransitions } from "next-view-transitions";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
 
@@ -29,27 +28,25 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <ViewTransitions>
-      <html lang="pt-br" className="dark" suppressHydrationWarning>
-        <body
-          className={`${poppins.className} px-2 ${poppins.variable} h-svh bg-gradient-to-r from-zinc-900 to-zinc-800 font-poppins text-zinc-50 antialiased selection:bg-emerald-400 selection:text-zinc-950`}
-        >
-          <Toaster />
-          {children}
-          <section>
-            <script
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{ __html: JSON.stringify(struttedData) }}
-            />
-            <script
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{
-                __html: JSON.stringify(struttedDataOrganization),
-              }}
-            />
-          </section>
-        </body>
-      </html>
-    </ViewTransitions>
+    <html lang="pt-br" className="dark" suppressHydrationWarning>
+      <body
+        className={`${poppins.className} px-2 ${poppins.variable} h-svh bg-gradient-to-r from-zinc-900 to-zinc-800 font-poppins text-zinc-50 antialiased selection:bg-emerald-400 selection:text-zinc-950`}
+      >
+        <Toaster />
+        {children}
+        <section>
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(struttedData) }}
+          />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(struttedDataOrganization),
+            }}
+          />
+        </section>
+      </body>
+    </html>
   );
 }
